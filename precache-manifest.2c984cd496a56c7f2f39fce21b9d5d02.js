a6c9a699b381f7207f85979777608c758f9544ca
Qiang <wzq.makeit@qq.com>
2019-08-04 15:19:42 +0800

    todolist first commit

---
+++ precache-manifest.2c984cd496a56c7f2f39fce21b9d5d02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ac366ca0dde260143421baa8f7835c",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "f1758810c3fcd8879019",
+    "url": "/todolist/static/css/main.87bff218.chunk.css"
+  },
+  {
+    "revision": "c0f92fbe2ad4c142db2f",
+    "url": "/todolist/static/js/2.59186d53.chunk.js"
+  },
+  {
+    "revision": "f1758810c3fcd8879019",
+    "url": "/todolist/static/js/main.beee64c6.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
